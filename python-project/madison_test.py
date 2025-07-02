7e8eee4e7c262468404ee192688b054ab4c9f61e
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-02 12:30:50 -0600

    Create madison_test.py

---
+++ python-project/madison_test.py
@@ -0,0 +1,20 @@
+from madison import main_program
+from timinator import CONGRATS
+import random
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+if __name__ == "__main__":
+    main_program()
+    success()
+    send_msg(f"{random.choice(CONGRATS)} 🌟", "You have given Madison a tremendous head start down her future path!!!")
