01034ac3fa84238cc663c4b647928ab857474768
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-15 17:57:38 -0600

    Update list_comprehension_ex_09_test.py

--- python-project/list_comprehension_ex_09_test.py
+++ python-project/list_comprehension_ex_09_test.py
@@ -59,59 +59,59 @@ if __name__ == "__main__":
 
 
 
-import builtins
+# import builtins
 
 
-sum_builtin_used = False
+# sum_builtin_used = False
 
 
-def new_sum(x):
-    global sum_builtin_used
-    sum_builtin_used = True
-    return orig_sum(x)
+# def new_sum(x):
+#     global sum_builtin_used
+#     sum_builtin_used = True
+#     return orig_sum(x)
 
 
-orig_sum = builtins.sum
-builtins.sum = new_sum
+# orig_sum = builtins.sum
+# builtins.sum = new_sum
 
 
-def send_msg(channel, msg):
-    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+# def send_msg(channel, msg):
+#     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
 
 
-def success():
-    print("TECHIO> success true")
+# def success():
+#     print("TECHIO> success true")
 
 
-def fail():
-    print("TECHIO> success false")
+# def fail():
+#     print("TECHIO> success false")
     
 
-def test_num_spaces():
-    try:
-        spaces = num_spaces('Barkley and Lulu need to go for a walk.')
-        assert spaces == 8, f"Trying num_spaces('Barkley and Lulu need to go for a walk.')... Expected 8, got {spaces}"
-        spaces = num_spaces('          ')
-        assert spaces == 10, f"Trying num_spaces('          ')... Expected 10, got {spaces}"
+# def test_num_spaces():
+#     try:
+#         spaces = num_spaces('Barkley and Lulu need to go for a walk.')
+#         assert spaces == 8, f"Trying num_spaces('Barkley and Lulu need to go for a walk.')... Expected 8, got {spaces}"
+#         spaces = num_spaces('          ')
+#         assert spaces == 10, f"Trying num_spaces('          ')... Expected 10, got {spaces}"
      
-        success()
+#         success()
 
-        if sum_builtin_used:
-            send_msg("My personal Yoda, you are. 🙏", "* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ")
-            send_msg("My personal Yoda, you are. 🙏", "           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ")
-            send_msg("My personal Yoda, you are. 🙏", "__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .")
-            send_msg("My personal Yoda, you are. 🙏", "'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　")
-            send_msg("My personal Yoda, you are. 🙏", " /'.-c    　   * ●  ¸.　　°     ° 　¸.    ")
-            send_msg("My personal Yoda, you are. 🙏", " |  /T      　　°     ° 　¸.     ¸ .　　  ")
-            send_msg("My personal Yoda, you are. 🙏", "_)_/LI")
-        else:
-            send_msg("Kudos 🌟", "Back to boring me to death...and I had so much hope for you.  Sigh.")
+#         if sum_builtin_used:
+#             send_msg("My personal Yoda, you are. 🙏", "* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ")
+#             send_msg("My personal Yoda, you are. 🙏", "           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ")
+#             send_msg("My personal Yoda, you are. 🙏", "__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .")
+#             send_msg("My personal Yoda, you are. 🙏", "'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　")
+#             send_msg("My personal Yoda, you are. 🙏", " /'.-c    　   * ●  ¸.　　°     ° 　¸.    ")
+#             send_msg("My personal Yoda, you are. 🙏", " |  /T      　　°     ° 　¸.     ¸ .　　  ")
+#             send_msg("My personal Yoda, you are. 🙏", "_)_/LI")
+#         else:
+#             send_msg("Kudos 🌟", "Back to boring me to death...and I had so much hope for you.  Sigh.")
 
-    except AssertionError as e:
-        fail()
-        send_msg("Oops! 🐞", e)
-        send_msg("Hint 💡", "Are you only counting the spaces? 🤔")
+#     except AssertionError as e:
+#         fail()
+#         send_msg("Oops! 🐞", e)
+#         send_msg("Hint 💡", "Are you only counting the spaces? 🤔")
 
 
-if __name__ == "__main__":
-    test_num_spaces()
+# if __name__ == "__main__":
+#     test_num_spaces()
