933e21eedd2d0ee8d2cdfa7872582a6a2be34167
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-15 13:16:45 -0600

    Update timinator.py

--- python-project/timinator.py
+++ python-project/timinator.py
@@ -1,3 +1,12 @@
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+def success():
+    print("TECHIO> success true")
+
+def fail():
+    print("TECHIO> success false")
+
 CONGRATS = ['Kudos!',
             'Well Done!',
             'Bravo!',
