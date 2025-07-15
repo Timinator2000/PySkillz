9b6f1e0bc2d97aa491e1da83f3be0884243469d6
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-15 16:21:19 -0600

    Update list_comp_remove_odds_test.py

--- python-project/list_comp_remove_odds_test.py
+++ python-project/list_comp_remove_odds_test.py
@@ -20,7 +20,7 @@ class RemoveOdds(timinator.Exercise):
         self.fixed_test_cases = [
             [[1, 3, 5, 7, 9]],
             [[2, 4, 6, 8, 10]],
-            [[],
+            [[]],
             [[1, 2, 3, 4, 5, 6]],
             [[25678, 435, 24, 999]]
         ]
