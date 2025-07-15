08bddcdd23dc59ee99e81b8c2f3b9aad65983148
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-15 13:32:50 -0600

    Update list_comp_remove_odds_test.py

--- python-project/list_comp_remove_odds_test.py
+++ python-project/list_comp_remove_odds_test.py
@@ -2,6 +2,14 @@ import timinator
 import random
 from list_comp_remove_odds import remove_odds
 
+
+suggested_solution = """
+
+def remove_odds(a_list: list[int]) -> list[int]:
+    return [i for i in a_list if i % 2 == 0]
+
+"""
+
 def test_remove_odds():
     try:
         answer = remove_odds([1, 3, 5, 7, 9])
@@ -17,6 +25,7 @@ def test_remove_odds():
      
         timinator.success()
         timinator.send_msg(f'{random.choice(timinator.CONGRATS)} 🌟', "Back to boring me to death...and I had so much hope for you.  Sigh.")
+        timinator.send_msg(f'Suggested Solution', suggested_solution)
 
     except AssertionError as e:
         timinator.fail()
