6d1b378e67aa327a74f5a32006034d13086d3a68
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-15 20:58:41 -0600

    Update list_comprehension_ex_09_test.py

--- python-project/list_comprehension_ex_09_test.py
+++ python-project/list_comprehension_ex_09_test.py
@@ -23,7 +23,7 @@ success_message += 'The debate over one space versus two spaces after a period '
 
                    'However...\n\n' + \
 
-                   'Using two spaces after a period can hint at someone's age, as it was ' + \
+                   'Using two spaces after a period can hint at someone\'s age, as it was ' + \
                    'standard in the typewriter era, common among those born before the 1980s. ' + \
                    'Younger generations, trained on computers with modern fonts, typically use ' + \
                    'one space, making two spaces a giveaway of older typing habits.'
@@ -63,64 +63,3 @@ class NumSpaces(timinator.Exercise):
 if __name__ == "__main__":
     exercise = NumSpaces()
     exercise.run()
-
-
-
-
-
-# import builtins
-
-
-# sum_builtin_used = False
-
-
-# def new_sum(x):
-#     global sum_builtin_used
-#     sum_builtin_used = True
-#     return orig_sum(x)
-
-
-# orig_sum = builtins.sum
-# builtins.sum = new_sum
-
-
-# def send_msg(channel, msg):
-#     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
-
-
-# def success():
-#     print("TECHIO> success true")
-
-
-# def fail():
-#     print("TECHIO> success false")
-    
-
-# def test_num_spaces():
-#     try:
-#         spaces = num_spaces('Barkley and Lulu need to go for a walk.')
-#         assert spaces == 8, f"Trying num_spaces('Barkley and Lulu need to go for a walk.')... Expected 8, got {spaces}"
-#         spaces = num_spaces('          ')
-#         assert spaces == 10, f"Trying num_spaces('          ')... Expected 10, got {spaces}"
-     
-#         success()
-
-#         if sum_builtin_used:
-#             send_msg("My personal Yoda, you are. 🙏", "* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ")
-#             send_msg("My personal Yoda, you are. 🙏", "           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ")
-#             send_msg("My personal Yoda, you are. 🙏", "__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .")
-#             send_msg("My personal Yoda, you are. 🙏", "'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　")
-#             send_msg("My personal Yoda, you are. 🙏", " /'.-c    　   * ●  ¸.　　°     ° 　¸.    ")
-#             send_msg("My personal Yoda, you are. 🙏", " |  /T      　　°     ° 　¸.     ¸ .　　  ")
-#             send_msg("My personal Yoda, you are. 🙏", "_)_/LI")
-#         else:
-#             send_msg("Kudos 🌟", "Back to boring me to death...and I had so much hope for you.  Sigh.")
-
-#     except AssertionError as e:
-#         fail()
-#         send_msg("Oops! 🐞", e)
-#         send_msg("Hint 💡", "Are you only counting the spaces? 🤔")
-
-
-# if __name__ == "__main__":
-#     test_num_spaces()
