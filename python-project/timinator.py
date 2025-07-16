b97ccacd2525f48e063b68273205cfe8d0d01e0a
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-15 18:17:02 -0600

    Update timinator.py

--- python-project/timinator.py
+++ python-project/timinator.py
@@ -126,6 +126,7 @@ class Exercise():
     def run_test_case(self, test_case):
         if PRINT_TEST_CASES:
             print(test_case)
+            print(*test_case)
                     
         expected_answer = self.solution(*test_case)
         user_answer =self.user_solution(*test_case)
