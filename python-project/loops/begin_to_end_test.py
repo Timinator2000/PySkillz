52132c70b6de86d87d29cd50fd36e823e66123fe
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-28 19:22:04 -0600

    Create begin_to_end_test.py

---
+++ python-project/loops/begin_to_end_test.py
@@ -0,0 +1,51 @@
+import random
+import timinator
+
+section, exercise_name = timinator.get_section_and_exercise_names(__file__)
+exec(f'from {exercise_name} import {exercise_name} as user_solution')
+exec(f'from {exercise_name}_solution import {exercise_name} as suggested_solution')
+
+
+success_message = """
+
+Q: 
+A: 
+
+Q: 
+A: 
+
+"""
+
+success_message += 'Fun fact:'
+
+
+
+class BeginToEnd(timinator.PrintBasedExercise):
+    
+    def __init__(self):
+        
+        super().__init__(user_solution, suggested_solution, f'{section}{exercise_name}_solution.py')
+        self.success_message = success_message
+        self.num_random_test_cases = 20
+
+        self.fixed_test_cases = [
+            [1, 5],
+            [-3, 0],
+            [-4, 4],
+            [5, -1],
+            [0, 4]
+        ]
+        
+        
+    def display_test_case(self, test_case) -> None:
+        begin, end = test_case
+        self.send_msg(self.bug_channel, f'   begin = {begin}     end = {end}')
+        
+        
+    def generate_random_test_case(self):
+        return [random.randint(-10, 10), random.randint(-10, 10)]
+
+
+if __name__ == "__main__":
+    exercise = BeginToEnd()
+    exercise.run()
