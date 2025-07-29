b242a2c609b7f1bf7f65ce12cf209ce8105acb2a
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-29 14:56:29 -0600

    Update begin_to_end.py

--- python-project/loops/begin_to_end.py
+++ python-project/loops/begin_to_end.py
@@ -1,4 +1,5 @@
 def begin_to_end(begin: int, end: int) -> None:
     
     # Write an answer using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    # To debug: import sys at the top of this script
+    #           print("Debug messages...", file=sys.stderr, flush=True)
