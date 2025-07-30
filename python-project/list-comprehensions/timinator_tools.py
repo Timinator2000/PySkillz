30341dd4a283e5be4a4574e2abd4ae6e3ad2928a
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-07-29 20:27:53 -0600

    Update timinator_tools.py

--- python-project/list-comprehensions/timinator_tools.py
+++ python-project/list-comprehensions/timinator_tools.py
@@ -105,7 +105,7 @@ class Exercise():
         if len(element_types) == 1:
             return f'[{element_types.pop()}]'
         
-        return ''
+        return '[]'
     
     
     def data_type(self, data) -> str:
