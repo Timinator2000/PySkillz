dbdbd3cf190cea5b3426f9aaa0e31e8a513dc63f
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-08-13 09:43:09 -0600

    Delete python-project/loops directory

--- python-project/loops/begin_to_end.py
+++
@@ -1,5 +0,0 @@
-def begin_to_end(begin: int, end: int) -> None:
-    
-    # Write an answer using print
-    # To debug: import sys at the top of this script
-    #           print("Debug messages...", file=sys.stderr, flush=True)
--- python-project/loops/begin_to_end_solution.py
+++
@@ -1,3 +0,0 @@
-def begin_to_end(begin: int, end: int) -> None:
-    for i in range(begin, end + 1):
-        print(i)
