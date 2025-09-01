8813b28d8cdf2428ce065c87e95aff5e8a506809
Timinator2000 <v0t2ugh@gmail.com>
2025-09-01 09:18:35 -0600

    Changed timinator_tools to pyskillz_tools

--- python-project/----new-exercises----/----exercise_template----/exercise_name_test.py
+++ python-project/----new-exercises----/----exercise_template----/exercise_name_test.py
@@ -13,11 +13,11 @@ dir_path, filename = os.path.split(os.path.normpath(__file__))
 sys.path.insert(0, os.path.join(dir_path, '..', '..', '----tools----'))
 
 try:
-    import timinator_tools
-    timinator_tools.check_for_tech_io(dir_path)
+    import pyskillz_tools
+    pyskillz_tools.check_for_tech_io(dir_path)
 
 except ImportError:
-    print(f'Import Error: timinator_tools.py needs to be in the tools folder, one level deep from python-project.')
+    print(f'Import Error: pyskillz_tools.py needs to be in the tools folder, one level deep from python-project.')
 
 exercise_name = filename[:filename.find('_test.py')]
 solution_filename = os.path.join(dir_path, f'{exercise_name}_solution.py')
@@ -40,8 +40,8 @@ success_message += ''
 success_message += ''
 
 
-class ExerciseName(timinator_tools.Exercise):
-class ExerciseName(timinator_tools.PrintBasedExercise):
+class ExerciseName(pyskillz_tools.Exercise):
+class ExerciseName(pyskillz_tools.PrintBasedExercise):
     
     def __init__(self):
 
